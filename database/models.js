993cbae13ff823dcfa93386382e0edf7d2c3c3ea
luoye <842891024@qq.com>
2017-01-08 21:57:12 +0800

    model design

--- database/models.js
+++ database/models.js
@@ -2,12 +2,106 @@ export default {
 	users: {
 		email: {
 			required: true,
-			type: String
+			type: 'string',
+			unique: true
 		},
 		password: {
 			required: true,
-			type: String
+			type: 'string'
 		},
-
+		createTime: {
+			required: true,
+			type: 'timestamp'
+		}
+	},
+	projects: {
+		name: {
+			required: true,
+			type: 'string'
+		},
+		description: {
+			type: 'string',
+		},
+		remark: {
+			type: 'string'
+		},
+		creat_by: {
+			required: true,
+			type: 'objectId'
+		},
+		update_by: {
+			required: true,
+			type: 'objectId'
+		},
+		create_at: {
+			required: true,
+			type: 'timestamp'
+		},
+		update_at: {
+			required: true,
+			type: 'timestamp'
+		},
+		members: {
+			required: true,
+			type: 'array'
+		},
+		private: {
+			required: true,
+			type: 'bool'
+		},
+		deleted: {
+			required: true,
+			type: 'bool'
+		}
+	},
+	apis: {
+		name: {
+			required: true,
+			type: 'string'
+		},
+		description: {
+			type: 'string'
+		},
+		creat_by: {
+			required: true,
+			type: 'objectId'
+		},
+		update_by: {
+			required: true,
+			type: 'objectId'
+		},
+		create_at: {
+			required: true,
+			type: 'timestamp'
+		},
+		update_at: {
+			required: true,
+			type: 'timestamp'
+		},
+		parent_project: {
+			required: true,
+			type: 'objectId'
+		},
+		request: {
+			url: String
+			method: String,
+			query: Array,
+			body: Array
+		},
+		response: {
+			required: true,
+			type: 'object'
+		},
+		remark: {
+			type: 'string'
+		},
+		status: {
+			required: true,
+			type: 'string'
+		},
+		deleted: {
+			required: true,
+			type: 'bool'
+		}
 	}
 };
